c40df5988c71a163ff369bdb748270e6fb31ecdd
Vicente <lazvinz21@gmail.com>
2023-08-24 19:55:24 -0500

    avatar relative path fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import "./navbar.scss";
 
-const Navbar = () =>{
+const Navbar = () => {
     return (
         <div className="navbar">
-        <div className="logo">
-            <div className="icons"></div>
-            <img src="logo.svg" alt="" />
-            <span>Vinz Admin</span>
+            <div className="logo">
+                <div className="icons"></div>
+                <img src="logo.svg" alt="" />
+                <span>Vinz Admin</span>
             </div><div className="icons">
                 <img src="/search.svg" alt="" className="icon" />
                 <img src="/app.svg" alt="" className="icon" />
@@ -16,9 +16,9 @@ const Navbar = () =>{
                     <span>1</span>
                 </div>
                 <div className="user">
-                    <img 
-                        src="src\imgs\Admin-Profile-Vector-PNG-Image.png" 
-                        alt="" 
+                    <img
+                        src="../../../public/Admin-Profile-Vector-PNG-Image.png"
+                        alt=""
                     />
                     <span>Vinz</span>
                 </div>
